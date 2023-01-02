fe30debc5e5ba526a87c9d4b18364336fb232e90
Rocky Petkov <rocky.petkov@yandex.com>
2023-01-02 01:50:29 -0500

    Adding a Neighbourhood Map to Learn React-Leaflet

--- src/map/layers.tsx
+++ src/map/layers.tsx
@@ -9,12 +9,11 @@ import {
 } from 'react-leaflet'
 import { Typography, Divider } from "@mui/material";
 import L from 'leaflet'
-import TO from '../data/ZoningArea.json'
-import { ZoneData } from '../models/zone'
+import Neighbourhoods from '../data/140NeighbourhoodsAugmented.json'
+import { NeighbourhoodData } from '../models/neighbourhoods'
 
 const Layers = () => {
-  // only load 50 zones to ease testing
-  const data = new ZoneData(TO, 50);
+  const data = new NeighbourhoodData(Neighbourhoods);
   const [borderData, setBorderData] = useState([data])
 
   const map = useMapEvents({
@@ -58,7 +57,9 @@ const Layers = () => {
           const output = []
           for (const elem of data.features){
             const geojson = elem.geometry
-            const name = String(elem.properties._id);
+            const name = String(elem.properties.AREA_NAME);
+            const percentSfh = Number(elem.properties.SINGLE_FAMILY_DWELLINGS / elem.properties.RESIDENTIAL_DWELLINGS)
+            const percentSfhStr = percentSfh.toPrecision(2)
             output.push((
               <>
                 <LayersControl.Overlay checked name={name}>
@@ -71,11 +72,17 @@ const Layers = () => {
                     >
                       <Popup>
                         <Typography variant='subtitle2'>
-                          IDX: {name}
+                          Neighbourhood: {name}
                         </Typography>
                         <Divider />
                         <Typography variant='body2' style={{ margin: 3 }}>
-                          Density: {elem.properties.DENSITY}
+                          Population: {elem.properties.POPULATION}
+                        </Typography>
+                        <Typography variant='body2' style={{ margin: 3 }}>
+                          Density: {elem.properties.DENSITY} persons / sq.km
+                        </Typography>
+                        <Typography variant='body2' style={{ margin: 3 }}>
+                          Single Family Residential Prevalence: {percentSfhStr}%
                         </Typography>
                       </Popup>
                     </GeoJSON>
---
+++ src/models/neighbourhoods.ts
@@ -0,0 +1,65 @@
+import { 
+    MultiPolygon,
+    FeatureCollection,
+    Feature,
+    GeoJsonProperties 
+} from 'geojson';
+
+interface Props {
+    _id: number;
+    AREA_ID: number;
+    AREA_ATTR_ID: number;
+    PARENT_AREA_ID: null;
+    AREA_SHORT_CODE: string;
+    AREA_LONG_CODE: string;
+    AREA_NAME: string;
+    AREA_DESC: string;
+    CLASSIFICATION: string;
+    CLASSIFICATION_CODE: string;
+    OBJECTID: string;
+    POPULATION: number;
+    DENSITY: number;
+    LAND_AREA: number;
+    RESIDENTIAL_DWELLINGS: number;
+    SINGLE_FAMILY_DWELLINGS: number;
+    MID_HIGH_RISE_APARTMENTS: number;
+    OTHER_ATTACHED_DWELLINGS: number;
+    SEMI_DETACHED_DWELLINGS: number;
+    ROW_HOUSES: number;
+    DUPLEX_UNITS: number;
+    LOW_RISE_APARTMENTS: number
+    OTHER_SINGLE_ATTACHED_DWELLING: number
+}
+
+interface FeatureTO extends Feature {
+    type: 'Feature';
+    properties: Props;
+    geometry: MultiPolygon;
+}
+
+interface CRS {
+    type: string;
+    properties: GeoJsonProperties;
+}
+
+export class NeighbourhoodData implements FeatureCollection {
+    type: 'FeatureCollection';
+    crs: CRS;
+    features: FeatureTO[];
+
+    constructor(input: any, n: number = -1) {
+        this.type = 'FeatureCollection';
+        this.crs = input.crs;
+
+        const temp = [];
+        let i = 0;
+        for (const elem of input.features){
+            temp.push(elem);
+            i = i + 1;
+            if (i == n) {
+                break;
+            }
+        }
+        this.features = temp;
+    }
+}

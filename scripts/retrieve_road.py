f08011863eb4a34e9304fd4f628efb3b578b3e83
sarodz <samirodrigue@hotmail.com>
2023-01-03 22:46:11 -0500

    fin

--- scripts/retrieve_road.py
+++ scripts/retrieve_road.py
@@ -2,4 +2,5 @@ import geopandas as gpd
 import osmnx
 
 boundary_geojson = gpd.read_file("./src/data/ZoningArea.json")
-osmnx.geometries.geometries_from_polygon(boundary_geojson.iloc[0].geometry, tags = {"highway": True})
+result = osmnx.geometries.geometries_from_polygon(boundary_geojson.iloc[0].geometry, tags = {"highway": True})
+result.to_json()

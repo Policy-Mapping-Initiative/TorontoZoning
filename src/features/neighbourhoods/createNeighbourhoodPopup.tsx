07c0231e2958a7f23ae8a70791e4e6c7c5ea9332
sarodz <samirodrigue@hotmail.com>
2023-03-03 21:37:15 -0500

    format

--- src/features/neighbourhoods/createNeighbourhoodPopup.tsx
+++ src/features/neighbourhoods/createNeighbourhoodPopup.tsx
@@ -5,17 +5,18 @@ import { getNeighbourhoods } from '../../utils/hooks';
 import { useAppSelector } from '../../utils/hooks';
 
 function perc2color(perc: number) {
-	var r, g, b = 0;
-	if(perc < 50) {
-		g = 255;
-		r = Math.round(5.1 * perc);
-	}
-	else {
-		r = 255;
-		g = Math.round(510 - 5.10 * perc);
-	}
-	var h = r * 0x10000 + g * 0x100 + b * 0x1;
-	return '#' + ('000000' + h.toString(16)).slice(-6);
+  var r,
+    g,
+    b = 0;
+  if (perc < 50) {
+    g = 255;
+    r = Math.round(5.1 * perc);
+  } else {
+    r = 255;
+    g = Math.round(510 - 5.1 * perc);
+  }
+  var h = r * 0x10000 + g * 0x100 + b * 0x1;
+  return '#' + ('000000' + h.toString(16)).slice(-6);
 }
 
 export const createPopup = () => {
